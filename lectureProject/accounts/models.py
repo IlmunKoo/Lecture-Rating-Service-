e4a6097b7bcecd5ff18608cd1666bcffe2b0f501
IlmunKoo <mun90505@naver.com>
2020-07-22 10:36:24 +0900

    삭제

--- lectureProject/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class Signup(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    std_num = models.CharField(max_length=50)
-    major = models.CharField(max_length=100)
-    name = models.CharField(max_length=200)
